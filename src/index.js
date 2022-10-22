a435f0ad84fd20fd2898ed0e56b7ac6d623a6cba
kcpele <fidekg123@gmail.com>
2022-10-22 23:18:52 +0100

    added search bar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { capsFirst } from "./utils";
 import "fontsource-inter/500.css";
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 import theme from "./theme";
 
 import {
@@ -14,19 +14,37 @@ import {
   HStack,
   Text,
   Flex,
-  Tag
+  Tag,
+  InputGroup,
+  InputLeftElement,
+  Input,
+  Spinner,
+  Box,
 } from "@chakra-ui/react";
 
 import ChakraCarousel from "./ChakraCarousel";
+import { Search2Icon } from "@chakra-ui/icons";
 
 function App() {
   const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [isLoaging, setIsLoading] = useState(false);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts/")
-      .then((res) => res.json())
-      .then((res) => setData(res));
-  }, []);
+    const getData = async () => {
+      setIsLoading(true);
+      let res = await fetch("https://jsonplaceholder.typicode.com/posts/");
+      let resData = await res.json();
+      let content = resData.filter(
+        (value) => value.userId.toString() === search
+      );
+      setData(content.length <= 0 ? resData : [...content]);
+      setIsLoading(false);
+      //can have an if statement if content is empty then show a notification no user
+      console.log(content);
+    };
+    getData();
+  }, [search, setSearch]);
 
   return (
     <ChakraProvider theme={extendTheme(theme)}>
@@ -39,62 +57,85 @@ function App() {
           md: "43.75rem",
           lg: "57.5rem",
           xl: "75rem",
-          xxl: "87.5rem"
+          xxl: "87.5rem",
         }}
       >
-        <ChakraCarousel gap={32}>
-          {data.slice(5, 15).map((post, index) => (
-            <Flex
-              key={index}
-              boxShadow="rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px"
-              justifyContent="space-between"
-              flexDirection="column"
-              overflow="hidden"
-              color="gray.300"
-              bg="base.d100"
-              rounded={5}
-              flex={1}
-              p={5}
-            >
-              <VStack mb={6}>
-                <Heading
-                  fontSize={{ base: "xl", md: "2xl" }}
-                  textAlign="left"
-                  w="full"
-                  mb={2}
-                >
-                  {capsFirst(post.title)}
-                </Heading>
-                <Text w="full">{capsFirst(post.body)}</Text>
-              </VStack>
+        <Box mb={10}>
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents="none"
+              children={<Search2Icon color="gray.300" />}
+            />
+            <Input
+              onChange={(e) => setSearch(e.target.value)}
+              value={search}
+              type="text"
+              placeholder="Search"
+            />
+          </InputGroup>
+        </Box>
+        {isLoaging ? (
+          <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="blue.500"
+            size="xl"
+          />
+        ) : (
+          <ChakraCarousel gap={32}>
+            {data.slice(5, 15).map((post, index) => (
+              <Flex
+                key={index}
+                boxShadow="rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px"
+                justifyContent="space-between"
+                flexDirection="column"
+                overflow="hidden"
+                color="gray.300"
+                bg="base.d100"
+                rounded={5}
+                flex={1}
+                p={5}
+              >
+                <VStack mb={6}>
+                  <Heading
+                    fontSize={{ base: "xl", md: "2xl" }}
+                    textAlign="left"
+                    w="full"
+                    mb={2}
+                  >
+                    {capsFirst(post.title)}
+                  </Heading>
+                  <Text w="full">{capsFirst(post.body)}</Text>
+                </VStack>
 
-              <Flex justifyContent="space-between">
-                <HStack spacing={2}>
-                  <Tag size="sm" variant="outline" colorScheme="green">
-                    User: {post.userId}
-                  </Tag>
-                  <Tag size="sm" variant="outline" colorScheme="cyan">
-                    Post: {post.id - 5}
-                  </Tag>
-                </HStack>
-                <Button
-                  onClick={() => alert(`Post ${post.id - 5} clicked`)}
-                  colorScheme="green"
-                  fontWeight="bold"
-                  color="gray.900"
-                  size="sm"
-                >
-                  More
-                </Button>
+                <Flex justifyContent="space-between">
+                  <HStack spacing={2}>
+                    <Tag size="sm" variant="outline" colorScheme="green">
+                      User: {post.userId}
+                    </Tag>
+                    <Tag size="sm" variant="outline" colorScheme="cyan">
+                      Post: {post.id - 5}
+                    </Tag>
+                  </HStack>
+                  <Button
+                    onClick={() => alert(`Post ${post.id - 5} clicked`)}
+                    colorScheme="green"
+                    fontWeight="bold"
+                    color="gray.900"
+                    size="sm"
+                  >
+                    More
+                  </Button>
+                </Flex>
               </Flex>
-            </Flex>
-          ))}
-        </ChakraCarousel>
+            ))}
+          </ChakraCarousel>
+        )}
       </Container>
     </ChakraProvider>
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
-
